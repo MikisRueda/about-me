184795b3491bb2d4ab4f889914f65df5f51bfd13
Miguel Rueda <msrr_2013@outlook.com>
2025-09-04 11:41:07 -0500

    Commit #1: Nombre

---
+++ resources/views/bio.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Document</title>
+    <!-- Tailwind CSS CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gray-200">
+    <div class="min-h-screen flex items-center justify-center">
+        <div class="bg-gray-100 p-4 w-full max-w-xl rounded shadow">
+            <div class="nombre text-center text-xl p-2">
+                <h1 class="font-semibold">Miguel Santiago Rueda</h1>
+                <div class="sub-nombre flex sm:flex-row items-center justify-center gap-3 text-sm pt-2">
+                    <p class="underline text-blue-500">correo</p>
+                    <p>fecha de nacimiento</p>
+                    <p>Pais,ciudad</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
