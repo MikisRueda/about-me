0756f600e2ae29f440fbd40b393873176057a892
Miguel Rueda <msrr_2013@outlook.com>
2025-09-04 12:28:54 -0500

    Commit #2: Lugar de nacimiento y procedencia

--- resources/views/bio.blade.php
+++ resources/views/bio.blade.php
@@ -11,13 +11,21 @@
 
 <body class="bg-gray-200">
     <div class="min-h-screen flex items-center justify-center">
-        <div class="bg-gray-100 p-4 w-full max-w-xl rounded shadow">
-            <div class="nombre text-center text-xl p-2">
+        <div class="bg-gray-100 p-4 w-full max-w-2xl rounded shadow">
+            <div class="text-center text-xl p-2">
                 <h1 class="font-semibold">Miguel Santiago Rueda</h1>
-                <div class="sub-nombre flex sm:flex-row items-center justify-center gap-3 text-sm pt-2">
-                    <p class="underline text-blue-500">correo</p>
-                    <p>fecha de nacimiento</p>
-                    <p>Pais,ciudad</p>
+                <div class="flex sm:flex-row items-center justify-center gap-3 text-sm pt-2">
+                    <p class="underline text-blue-500">msrr_2013@outlook.com</p>
+                    <p>28 septiembre 2002</p>
+                    <p>Colombia, Bucaramanga</p>
+                </div>
+                <hr class="my-4 border-gray-300" />
+                <div class="flex flex-col justify-center gap-2 text-sm">
+                    <h2 class="text-lg text-left font-semibold">Biografia</h2>
+                    <p class="text-left">Nací en Bucaramanga, soy originario de Colombia. Mis padres se llaman Miguel Vicente y Adriana
+                        Marcela y tengo una
+                        hermana llamada valeria. Crecí en un ambiente familiar lleno de amor y respeto.
+                    </p>
                 </div>
             </div>
         </div>
